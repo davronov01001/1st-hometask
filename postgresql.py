733fa14a58dcc111df046fd3abfc57967a14679b
davronov01001 <163631486+davronov01001@users.noreply.github.com>
2024-06-22 14:26:33 +0500

    Create postgresql.py

---
+++ postgresql.py
@@ -0,0 +1,25 @@
+"""postgresql"""
+
+
+# -- drop table if exists employee;
+# -- create table MOCK_DATA(
+# -- 	id serial primary key,
+# -- 	first_name VARCHAR(50),
+# -- 	last_name VARCHAR(50),
+# -- 	email VARCHAR(50),
+# -- 	gender VARCHAR(50),
+# -- 	country VARCHAR(50),
+# -- 	phone VARCHAR(50),
+# -- 	active boolean,
+# -- 	birth_Date DATE
+# -- );
+
+# -- select first_name, last_name, phone, country, gender from MOCK_DATA;
+# -- update mock_data set gender = 'Male' where gender != 'Male' or gender !='Female';
+
+# -- select first_name,last_name,country,gender from mock_data where first_name like 'An%';
+
+# -- select first_name,last_name,country,age from mock_data where age between 40 and 60;
+
+# --select title, price, quantity, SUM(price*quantity) as summa from tovar group by title, price, quantity order by summa desc limit 10;
+
